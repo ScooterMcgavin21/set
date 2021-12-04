da66b23f413f9181a7f57a3e682d3a8dfa5aeafc
ScooterMcgavin21 <Scott.skrobel@gmail.com>
2021-12-04 16:24:46 -0700

    load api data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tokens: [],
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData = async () => {
+    const response = await fetch('https://api.tokensets.com/public/v2/portfolios');
+    if (response) {
+      const data = await response.json();
+      console.log(response);
+      console.log(data.portfolios);
+      // console.log(allusers[0]);
+      this.setState({
+        isLoaded: true,
+        tokens: [...data.portfolios],
+      });
+    }
+  };
+
+  render() {
+    let { isLoaded, tokens } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="userlist">
+          <ul>
+            {tokens.map(token => (
+              <li key={token.id}>
+                {token.symbol} {token.name} {token.address} 
+              </li>
+            ))} 
+          </ul>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
